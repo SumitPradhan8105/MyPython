75c61db6560f0f365d4cd2d9fa43889cffce7ded
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-01-05 08:40:05 +0530

    Add files via upload

---
+++ Swap 2 Numbers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+
+print("Value of a : ",a,"\t","Value of b : ",b)
+
+temp=a
+a=b
+b=temp
+
+print("\nAfter Swap : \n")
+
+print("Value of a : ",a,"\t","Value of b : ",b)
+
+
+# In[ ]:
+
+
+
+
