109d15917b8830ccc4cc59efbd09591ec5142826
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-01-05 10:24:16 +0530

    Add files via upload

---
+++ user defined list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+lst = []
+n = int(input("Enter the size of list :"))
+
+for i in range (0,n):
+    x = int(input())
+    lst.append(x)
+    
+print(lst)
+
+        
+
+
+# In[ ]:
+
+
+
+
