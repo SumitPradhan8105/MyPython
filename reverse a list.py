a9a68a96c9ff882121ae7a89d63e35d1cb84f486
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-01-05 09:51:46 +0530

    Add files via upload

---
+++ reverse a list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def reverse(newlist):
+    print(" Entered list is : ", newlist)
+    i = len(newlist) - 1
+    
+    while i >= 0 :
+        print(newlist[i])
+        i -= 1
+    
+
+
+
+
+list = [1,2,3,4,5]
+reverse(list)
+
+
+# In[ ]:
+
+
+
+
