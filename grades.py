d04b75c1a81d1ec45b6b08eec4552a75fecb9976
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-01-05 10:04:34 +0530

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+marks = int(input("Enter your marks : "))
+
+if marks >= 90 :
+    print("You Got 'A' Grade !")
+elif marks >= 80 :
+    print("You Got 'B' Grade !")
+elif marks >= 70 :
+    print("You Got 'C' Grade !")
+elif marks >= 60 :
+    print("You Got 'D' Grade !")
+elif marks >= 50 :
+    print("You Got 'E' Grade !")
+else:
+    print("You have failed the exam!")
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
