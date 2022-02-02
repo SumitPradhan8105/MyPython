5e088bee34c703ebc0b4d9e28b6011079858588e
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-02 11:27:13 +0530

    Create iterating in sets.py

---
+++ iterating in sets.py
@@ -0,0 +1,6 @@
+Days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}    
+print(Days)    
+print(type(Days))    
+print("Iterating in sets ")    
+for i in Days:    
+    print(i)   
