b791cdfba654465e9521f80b471cb26ade3346c3
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-02 11:53:27 +0530

    Create while loop.py

---
+++ while loop.py
@@ -0,0 +1,15 @@
+n = int(input("Enter n: "))
+
+
+
+# initialize sum and counter
+sum = 0
+i = 1
+
+while i <= n:
+    print(i)
+    sum = sum + i
+    i+=1
+
+# print the sum
+print("The sum is", sum)
