113985b55c86c57906fd4d1c131f1275643d624e
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-02 11:12:20 +0530

    Create accessing elements in dict.py

---
+++ accessing elements in dict.py
@@ -0,0 +1,8 @@
+Employee = {"Name": "John", "Age": 29, "salary":25000,"Company":"GOOGLE"}  
+print(type(Employee))  
+print("printing Employee data .... ")  
+print("Name : " ,Employee["Name"])  
+print("Age : " ,Employee["Age"])  
+print("Salary : " ,Employee["salary"])  
+print("Company : " ,Employee["Company"])  
+#accessing elements in dictionary
