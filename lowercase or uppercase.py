63dc53076cda4fd98eb0d892c6b604d7cdaeb8a6
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-02 12:08:16 +0530

    Create lowercase or uppercase.py

---
+++ lowercase or uppercase.py
@@ -0,0 +1,14 @@
+ch = input("Enter Character : ")
+
+if(ch.islower()):
+    print(" Character ", ch, "is a Lowercase Alphabet")
+else:
+    print(" ", ch, "is Not a Lowercase Alphabet")
+    
+    
+ch1 = input("Please Enter Your Own Character : ")
+
+if(ch1.isupper()):
+    print("The Given Character ", ch1, "is an Uppercase Alphabet")
+else:
+    print("The Given Character ", ch1, "is Not an Uppercase Alphabet")
