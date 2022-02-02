fd3461d2d2ff99e9e5ad984b19ef594427fe1a0e
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-02 11:10:40 +0530

    Create item match.py

---
+++ item match.py
@@ -0,0 +1,9 @@
+list =[5,3,4,2,1,6]  
+count = 0;  
+for i in list:
+    count = count+1
+    if i == 4:  
+        print("item matched")  
+         
+        break  
+print("found at",count,"location")
