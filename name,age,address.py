3b3701bc22a9d94004ee69af821c0711e4114e07
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-12 16:47:27 +0530

    Create name,age,address.py

---
+++ name,age,address.py
@@ -0,0 +1,5 @@
+#. Write a Python program to display your details like name, age, address in three different lines.
+dict = {"name":"Sumit","class":12,"Address":"Mahanadi Vihar,Cuttack"}
+print("Name :",dict["name"])
+print("Class :",dict["class"])
+print("Address :",dict["Address"])
