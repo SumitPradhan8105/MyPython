eb6e3df82e3859a773fa1e53ef6f85148256073f
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-12 16:49:29 +0530

    Create mid point of line.py

---
+++ mid point of line.py
@@ -0,0 +1,11 @@
+#Find the mid point of a line
+x1 = float(input('The value of x1 : '))
+y1 = float(input('The value of y1 : '))
+
+x2 = float(input('The value of x2 : '))
+y2 = float(input('The value of y2 : '))
+
+x_m_point = (x1 + x2)/2
+y_m_point = (y1 + y2)/2
+coord = (x_m_point,y_m_point)
+print("The midpoint of line is :",coord)
