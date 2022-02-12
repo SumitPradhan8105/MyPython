3e2d4479b424a8771599f80e1f063420789f251e
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-12 16:49:10 +0530

    Create sort 3 numbers without loop or if else.py

---
+++ sort 3 numbers without loop or if else.py
@@ -0,0 +1,12 @@
+#Write a Python program to sort three integers without using conditional statements and loops.
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+z = int(input("Enter third number:"))
+
+a1 = min(x,y,z)
+a3 = max(x,y,z)
+
+a2 = (x+y+z)-a1-a3
+
+print("Sorted Elements: ",a1,a2,a3)
