575351c92766862e5809b382aa6966a5c69f017b
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-12 16:46:05 +0530

    Create area of triangle.py

---
+++ area of triangle.py
@@ -0,0 +1,5 @@
+# Write a Python program that will accept the base and height of a triangle and compute the area
+b = float(input("Enter the base: "))
+h = float(input("Enter the height: "))
+
+print("Area of triangle: ",0.5*b*h)
