cc97eda8b53bdcf2b882c605ba83a463f0cef20a
SumitPradhan8105 <73753526+SumitPradhan8105@users.noreply.github.com>
2022-02-12 16:44:36 +0530

    Create add Is to string.py

---
+++ add Is to string.py
@@ -0,0 +1,6 @@
+#Write a Python program to get a new string from a given string where "Is" has been added to the front. If the given string already begins with "Is" then return the string unchanged.
+string = input("Enter string")
+if string[:2] == 'Is':
+    print(string)
+else:
+    print('Is'+string)
